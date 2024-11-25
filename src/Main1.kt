656039adedac4c610df5be3a99c7f8872d70e74b
jd <tal05283@educastur.es>
2024-11-25 19:25:59 +0100

    Tarea1Arreglos2

--- src/Main1.kt
+++ src/Main1.kt
@@ -1,15 +1,14 @@
 fun main() {
 
-    for (num in 1 .. 100){
+    for (num in 1 .. 100) {
 
-        if (num%3 == 0 && num%5 == 0){
+        if (num % 3 == 0 && num % 5 == 0) {
             println("triqui")
-        }
-        else if (num%3 == 0){
+        } else if (num % 3 == 0) {
             println("tri")
-        }else if(num%5 == 0){
+        } else if (num % 5 == 0) {
             println("qui")
-        }else {
+        } else {
             println(num)
 
         }
