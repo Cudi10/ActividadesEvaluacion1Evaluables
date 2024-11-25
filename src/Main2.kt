a908aef5403965289cd69bd7a172c79bc4269ac6
jd <tal05283@educastur.es>
2024-11-25 19:24:57 +0100

    Tarea2Arreglos2

--- src/Main2.kt
+++ src/Main2.kt
@@ -1,11 +1,18 @@
 fun main(){
 
-}
-
-private fun Anagramas(){
-
     println("Introduzca una palabra:")
 
     println("Introduzca otra palabra:")
 
+}
+
+fun Anagramas(palabra1: String, palabra2: String): Boolean {
+    val pala1 = palabra1.replace(" ", "").lowercase()
+    val pala2 = palabra2.replace(" ", "").lowercase()
+
+    if (pala1.length != pala2.length) {
+        return false
+    }
+
+    return pala1.toList().sorted() == pala2.toList().sorted()
 }
