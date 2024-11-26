9ef8558e801b455d74bd5cb94cf26d206dc7d9e6
jd <tal05283@educastur.es>
2024-11-26 18:55:49 +0100

    AvanzandoTarea3

--- src/Main3.kt
+++ src/Main3.kt
@@ -1,3 +1,33 @@
 fun main(){
 
 }
+
+fun ExpresionBalanceada(expresion: String): Boolean {
+    val pila = mutableListOf<Char>()
+
+    for (caracter in expresion) {
+        when (caracter) {
+            '(', '{', '[' -> {
+                pila.add(caracter)
+            }
+
+            ')', '}', ']' -> {
+                if (pila.isEmpty()) {
+                    return false
+                }
+
+                val top = pila.removeAt(pila.size - 1)
+
+                if (((caracter == ')') && (top != '(')) ||
+                    ((caracter == '}') && (top != '{')) ||
+                    ((caracter == ']') && (top != '['))
+                ) {
+                    return false
+                }
+            }
+        }
+    }
+
+    return pila.isEmpty()
+
+}
