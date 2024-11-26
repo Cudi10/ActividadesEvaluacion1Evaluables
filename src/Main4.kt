c043cd53aed98e690185055d7e5c9088b3634f83
jd <tal05283@educastur.es>
2024-11-26 19:27:52 +0100

    Avanzando1Tarea4

--- src/Main4.kt
+++ src/Main4.kt
@@ -1,3 +1,10 @@
 fun main(){
-
+    val expresion1 = arrayListOf<String>("correr", "saltar", "correr", "saltar", "correr")
+    val expresion2 = arrayListOf<String>("correr", "correr", "correr", "saltar", "correr")
+    val expresion3 = arrayListOf<String>("correr", "correr", "saltar", "saltar", "correr")
+    val expresion4 = arrayListOf<String>("correr", "correr", "saltar", "saltar", "correr")
+    val expresion5 = arrayListOf<String>("correr", "saltar", "correr", "saltar")
+    val expresion6 = arrayListOf<String>("correr", "saltar", "correr", "saltar", "correr", "saltar", "correr")
+    val expresion7 = arrayListOf<String>("saltar", "saltar", "saltar", "saltar", "saltar")
+    val expresion8 = arrayListOf<String>("saltar", "saltar", "saltar", "saltar", "saltar")
 }
