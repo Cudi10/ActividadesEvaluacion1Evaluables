e4ff1d2fd6d0321641d2d153d750cd1130abaf7c
jd <tal05283@educastur.es>
2024-11-27 19:44:28 +0100

    Terminado?Tarea5

--- src/Main5.kt
+++ src/Main5.kt
@@ -1,24 +1,23 @@
-fun main(){
-
-    val matriz1= arrayOf(
+fun main() {
+    val matriz1 = arrayOf(
         arrayOf("X", "O", "X"),
         arrayOf("O", "X", "O"),
         arrayOf("O", "O", "X")
     )
 
-    val matriz2= arrayOf(
+    val matriz2 = arrayOf(
         arrayOf("", "O", "X"),
         arrayOf("", "X", "O"),
         arrayOf("", "O", "X")
     )
 
-    val matriz3= arrayOf(
+    val matriz3 = arrayOf(
         arrayOf("O", "O", "O"),
         arrayOf("O", "X", "X"),
         arrayOf("O", "X", "X")
     )
 
-    val matriz4= arrayOf(
+    val matriz4 = arrayOf(
         arrayOf("X", "O", "X"),
         arrayOf("X", "X", "O"),
         arrayOf("X", "X", "X")
@@ -28,14 +27,13 @@ fun main(){
     println(Matriz(matriz2))
     println(Matriz(matriz3))
     println(Matriz(matriz4))
-
 }
 
-fun Matriz(matriz: Array<Array<String>>): String{
-
+fun Matriz(matriz: Array<Array<String>>): String {
     var countX = 0
     var countO = 0
     var countVacios = 0
+
 
     for (i in 0..2) {
         for (j in 0..2) {
@@ -43,10 +41,39 @@ fun Matriz(matriz: Array<Array<String>>): String{
                 "X" -> countX++
                 "O" -> countO++
                 "" -> countVacios++
-                else -> return "Nulo"
+                else -> return "NulL"
             }
         }
     }
 
-    return TODO("Provide the return value")
+
+    fun esGanador(symbol: String): Boolean {
+        for (i in 0..2) {
+            if (matriz[i].all { it == symbol } || matriz.all { it[i] == symbol }) {
+                return true
+            }
+        }
+
+        if ((matriz[0][0] == symbol && matriz[1][1] == symbol && matriz[2][2] == symbol) ||
+            (matriz[0][2] == symbol && matriz[1][1] == symbol && matriz[2][0] == symbol)) {
+            return true
+        }
+
+        return false
+    }
+
+    val ganoX = esGanador("X")
+    val ganoO = esGanador("O")
+    val empate = esGanador("Empate")
+    val nulo = esGanador("Null")
+
+    if (ganoX) return "X"
+
+    if (ganoO) return "O"
+
+    if (empate == (countX + countO == 9)) return "Empate"
+
+    if (nulo == ganoX && ganoO) return "NulL"
+
+    return "NulL"
 }
